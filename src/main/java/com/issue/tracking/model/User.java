459566b86e7515b7e426e1a1be37288a0fe47853
sophannydun <sophannydun@gmail.com>
2018-04-21 21:29:57 +0700

    CRUD user management

--- src/main/java/com/issue/tracking/model/User.java
+++ src/main/java/com/issue/tracking/model/User.java
@@ -1,5 +1,104 @@
 package com.issue.tracking.model;
 
+import java.util.List;
 public class User {
+	private int userId;
+	private String username;
+	private String password;
+	private String createBy;
+	private String updateBy;
+	private String createDate;
+	private String updateDate;
+	private Department department;
+	private Branch branch;
+	private List<Role> roles;
+	
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCreateBy() {
+		return createBy;
+	}
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+	public String getUpdateBy() {
+		return updateBy;
+	}
+	public void setUpdateBy(String updateBy) {
+		this.updateBy = updateBy;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
 
+	public List<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+	
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	public Branch getBranch() {
+		return branch;
+	}
+	public void setBranch(Branch branch) {
+		this.branch = branch;
+	}
+	
+	public User() {
+		super();
+	}
+	public User(int userID, String username, String password, String createBy, String updateBy, String createDate,
+			String updateDate, Department department, Branch branch, List<Role> roles) {
+		super();
+		this.userId = userID;
+		this.username = username;
+		this.password = password;
+		this.createBy = createBy;
+		this.updateBy = updateBy;
+		this.createDate = createDate;
+		this.updateDate = updateDate;
+		this.department = department;
+		this.branch = branch;
+		this.roles = roles;
+	}
+	@Override
+	public String toString() {
+		return "User [userID=" + userId + ", username=" + username + ", password=" + password + ", createBy=" + createBy
+				+ ", updateBy=" + updateBy + ", createDate=" + createDate + ", updateDate=" + updateDate
+				+ ", department=" + department + ", branch=" + branch + ", roles=" + roles + "]";
+	}
+	
 }
