1572ec15b6896b1a8ca1ef47a2cc89794561a00a
NarakHokUP <hnarak@puthisastra.edu.kh>
2018-04-29 05:35:58 +0700

    fix login style

--- src/main/java/com/issue/tracking/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/issue/tracking/configuration/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -27,33 +28,47 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(userDetailsService);
 	}
 
+	// To enable style
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/static/**", "/resources/templates/**");
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// redirect user to formLogin if user trying to access protected
 		// resource
 
-		// To open from login
+		// To enable style
+		http
+			.authorizeRequests()
+			.antMatchers("/css/**").permitAll();
+		// To open from login	
 		http.formLogin()
-				 /*disabled .usernameParameter and .passwordParameter coz it is default
-				if wrong binding name mush enable both*/
+				/*
+				 * disabled .usernameParameter and .passwordParameter coz it is default if wrong
+				 * binding name mush enable both
+				 */
 				// .usernameParameter("username")
 				// .passwordParameter("password")
-		
+
 				// custom login page
+
 				.loginPage("/login").permitAll()
+				
+
 				/*
-				 * If .defaultSuccessUrl is not working must implement from
-				 * SuccessHandler interface
+				 * If .defaultSuccessUrl is not working must implement from SuccessHandler
+				 * interface
 				 * 
-				 * @Autowired private AuthenticationSuccessHandler
-				 * successHandler;
+				 * @Autowired private AuthenticationSuccessHandler successHandler;
 				 */
 				// .defaultSuccessUrl("/index1")
 				.successHandler(successHandler);
 		// logout
 		/*
-		 * http.logout() .logoutRequestMatcher(new
-		 * AntPathRequestMatcher("/logout")) .logoutSuccessUrl("/");
+		 * http.logout() .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		 * .logoutSuccessUrl("/");
 		 */
 
 		/******
@@ -62,8 +77,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 		// http.httpBasic();
 
 		/*
-		 * disable token generate from server for client that don't have token
-		 * like mobile for web service
+		 * disable token generate from server for client that don't have token like
+		 * mobile for web service
 		 */
 		http.csrf().disable();
 
@@ -72,8 +87,9 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().anyRequest().authenticated();
 		/* .antMatchers("/api/**").hasRole("admin"); */
 
-		/***** Below use only with API ***** API not Store Session or anything that
-		 * is STATELESS API
+		/*****
+		 * Below use only with API ***** API not Store Session or anything that is
+		 * STATELESS API
 		 * http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		 */
 	}
